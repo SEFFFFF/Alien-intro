ed1c732e4f6eaffe2a995bd81942b6a9bc5d35a2
Sef van Halbeek <s.vanhalbeek@student.fontys.nl>
2021-09-10 13:55:05 +0200

    V0.3 Added function to loop text

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    const crew=[1, 2, 3, 4, 5, 6, 7, 8];
+    let crew_index=0;
 
     function hide_text(){
         const text=document.querySelectorAll(".test>div")
         text.forEach((textelement)=>{
             textelement.classList.remove("visible");
+            
         });
     };
+   
+    function show_text(){
+        hide_text();
+        crew_index++;
+        setTimeout(hide_text, 4000);
+        if(crew.length>=crew_index){            
+            const member=document.querySelector(".test>div[data-order='"+crew_index+"']");
+            member.classList.add("visible");
+            setTimeout(show_text, 6000);
+        } else {
+            stop_button.click();
+        }
+    }
 
+    show_text();
 });
